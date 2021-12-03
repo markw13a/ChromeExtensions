3178c1b5400c137593c26b153d1d112c75b7a8c0
markw13a <markw13a@gmail.com>
2021-12-03 19:24:24 +0000

    Refactor in preparation for working on multiple pages

--- LinkedInVisaSponsors/background.js
+++ LinkedInVisaSponsors/background.js
@@ -1,3 +1,4 @@
+const sponsorElementClassname = "artdeco-entity-lockup__visa-sponsor";
 const sponsorIconURL = chrome.runtime.getURL('assets/sponsor-icon.svg');
 let visaSponsors = [];
 const loadVisaSponsor = async () => {
@@ -12,6 +13,46 @@ loadVisaSponsor();
 // If sponsor, edit element
 
 // Element structure different for different pages
+
+// job-card-container
+setInterval(() => {
+    const isOnJobsPage = window.location.href.endsWith("/jobs/");
+    const isOnJobSearchPage = window.location.href.includes("/jobs/search/");
+    const isOnJobViewPage = window.location.href.includes("/jobs/view/");
+
+    const jobContainers = Array.from(document.querySelectorAll(".jobs-search-results__list-item"));
+    const sponsors = jobContainers.reduce((arr, jobContainer) => {
+        const container = jobContainer.querySelector(".job-card-container__company-name");
+        const companyName = container?.textContent?.trim();
+        const isCompanyAVisaSponsor = !!visaSponsors.find(sponsor => sponsor.includes(companyName));
+
+        if (isCompanyAVisaSponsor) {
+            arr.push(jobContainer);
+        }
+
+        return arr;
+    }, []);
+
+    sponsors.forEach((element) => {
+        // "job-flavours" are supposed to present a single aspect of the job that may pique an applicant's interest
+        // As there is only ever meant to be one of these shown at a time, we delete the existing job-flavors list in favour of showing the visa sponsor icon
+        // https://engineering.linkedin.com/blog/2016/08/job-flavors-at-linkedin--part-i
+        const jobFlavours = element.querySelector(".job-card-list__insight");
+        if (jobFlavours) {
+            jobFlavours.style = "display: none !important;";
+        }
+
+        // Don't re-add the sponsor element if it's already been added
+        // Remember, we're running this twice every second!
+        const isVisaSponsorElementAlreadyPresent = element.querySelector(`.${sponsorElementClassname}`);
+        if (!isVisaSponsorElementAlreadyPresent) {
+            // Element that contains job title, company name, etc.
+            const jobContentElement = element.querySelector('.artdeco-entity-lockup__content');
+            const visaSponsorElement = createVisaSponsorElement();
+            jobContentElement.appendChild(visaSponsorElement);
+        }
+    })
+}, 500);
 
 const createVisaSponsorElement = () => {
     const visaSponsorContainer = document.createElement("div");
@@ -37,42 +78,3 @@ const createVisaSponsorElement = () => {
 
     return visaSponsorContainer;
 };
-
-// job-card-container
-setInterval(() => {
-    const isOnJobsPage = window.location.href.endsWith("/jobs/");
-    const isOnJobSearchPage = window.location.href.includes("/jobs/search/");
-    const isOnJobViewPage = window.location.href.includes("/jobs/view/");
-
-    const sponsorElementClassname = "artdeco-entity-lockup__visa-sponsor";
-    const jobContainers = Array.from(document.querySelectorAll(".jobs-search-results__list-item"));
-
-    const sponsors = jobContainers.reduce((arr, jobContainer) => {
-        const container = jobContainer.querySelector(".job-card-container__company-name");
-        const companyName = container?.textContent?.trim();
-        const isCompanyAVisaSponsor = !!visaSponsors.find(sponsor => sponsor.includes(companyName));
-
-        if (isCompanyAVisaSponsor) {
-            arr.push(jobContainer);
-        }
-
-        return arr;
-    }, []);
-    // Don't re-add the sponsor element if it's already part of the job card
-    const elementsToAddSponsorContentTo = sponsors.filter(sponsor => !sponsor.querySelector(`.${sponsorElementClassname}`));
-
-    elementsToAddSponsorContentTo.forEach((element) => {
-        // "job-flavours" are supposed to present a single aspect of the job that may pique an applicant's interest
-        // As there is only ever meant to be one of these shown at a time, we delete the existing job-flavors list in favour of showing the visa sponsor icon
-        // https://engineering.linkedin.com/blog/2016/08/job-flavors-at-linkedin--part-i
-        const jobFlavours = element.querySelector(".job-card-list__insight");
-        if (jobFlavours) {
-            jobFlavours.style = "display: none !important;";
-        }
-
-        // Element that contains job title, company name, etc.
-        const jobContentElement = element.querySelector('.artdeco-entity-lockup__content');
-        const visaSponsorElement = createVisaSponsorElement();
-        jobContentElement.appendChild(visaSponsorElement);
-    })
-}, 500);
